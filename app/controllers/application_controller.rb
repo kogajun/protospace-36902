c5e9e781c4b7e7652141d875a0468792f424a94c
kogajun <jannoir890118@yahoo.co.jp>
2021-09-27 18:59:27 +0900

    新規登録修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_partameters, if: :devise_controller?
 
   private
   def configure_permitted_partameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
   end
 end
